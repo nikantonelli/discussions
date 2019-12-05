ba7bc99b5992158b810d98641c300fa5ff205996
Nik <nik@nikantonelli.com>
2019-12-05 16:34:40 +0000

    First pass

--- App.js
+++ App.js
@@ -4,76 +4,132 @@ Ext.define('CustomApp', {
     
 launch: function() {
     var that =  this;
-        var millisecondsInDay = 86400000;
-        var currentDate = new Date();
-        var startDate = new Date(currentDate - millisecondsInDay*30); //in the last 30 days
-        var startDateUTC = startDate.toISOString();
-        
-        this._filters = [
+    var minutesInDay = 1440;
+    var daysToSee = 14;
+    
+//    this._filters = [];
+    this._filters = [
             {
-                property : 'CreationDate',
-                operator : '>=',
-                value : startDateUTC
-                
-            }	
+            property : 'LastUpdateDate',
+            operator : '>=',
+            value : Ext.Date.subtract(new Date(), Ext.Date.MINUTE, minutesInDay * daysToSee)
+            
+        }	
    	];
         
-        Ext.create('Rally.data.wsapi.Store',{
-	    model: 'ConversationPost',
+    Ext.create('Rally.data.wsapi.artifact.Store',{
+        models: ['UserStory'],
+        context: this.getContext().getDataContext(),
 	    autoLoad: true,
-	    remoteSort: false,
-	    fetch: ['Artifact','Text','CreationDate', 'User', 'FormattedID', 'ScheduleState', 'PlanEstimate'],
+	    // sorters: [
+        // {
+        //     property: 'CreationDate',
+        //     direction: 'DESC'
+        // }
+        // ],
+	    fetch: ['FormattedID','Discussion', 'LatestDiscussionAgeInMinutes','CreationDate', 'Name', 'State', 'ScheduleState', 'Owner', 'PlanEstimate'],
 	    filters: this._filters,
 	    listeners: {
-		load: that._onConversationsLoaded,
+		load: that._onArtifactsLoaded,
 		scope:this
 	    }
    	});
 
    },
-   _onConversationsLoaded: function(store,data){
-        var posts = [];
-        _.each(data, function(post) {
-            var p  = {
-                User: post.get('User')._refObjectName,
-                Text: post.get('Text'),
-                CreationDate: post.get('CreationDate'),
-                Name: post.get('Artifact').FormattedID,
-                Artifact: post.get('Artifact').FormattedID,
-                ScheduleState: post.get('Artifact').ScheduleState,
-                PlanEstimate: post.get('Artifact').PlanEstimate
-            };
-            posts.push(p);
-            });
-        this._createGrid(posts);
+   _onArtifactsLoaded: function(store,data){
+        //Sort the records into the order of most recent post.
+        // this._createGrid(_.sortBy( data, function(record) {
+        //     return record.get('LatestDiscussionAgeInMinutes')
+        // }));
+
+        store.sort( {
+            property: 'LatestDiscussionAgeInMinutes',
+            direction: 'ASC'
+        });
+        this._createGrid(store.getRecords());
    },
    
-   _createGrid: function(posts) {
+   _createGrid: function(data) {
+
+        _.each(data, function(record) {
+            record.lastPost = '';
+        });
+
         _store = Ext.create('Rally.data.custom.Store', {
-                data: posts,
-                groupField: 'Artifact',
+                data: data,
+                groupField: 'FormattedID',
                 pageSize: 100
             });
         if (!this.grid) {
         this.grid = this.add({
+            margin: 20,
             xtype: 'rallygrid',
             itemId: 'mygrid',
             store: _store,
-            features: [{ftype:'grouping'}],
+            plugins: [{
+                ptype: 'rowexpander',
+                rowBodyTpl : new Ext.XTemplate(
+                    '{record:this.getRenderer}',
+                    {
+                        getRenderer: function(record) {
+                            // debugger;
+                            return Rally.ui.renderer.RendererFactory.getRenderTemplate('Discussions');
+                        }
+                    }
+                )
+            }],
+            collapsible: true,
             columnCfgs: [
                 {
-                   text: 'Artifact', dataIndex: 'Artifact'
+                   text: 'Artifact', dataIndex: 'FormattedID'
                 },
+                {text: 'Title', dataIndex: 'Name', flex: 1},
                 {text: 'Schedule State', dataIndex: 'ScheduleState'},
-                {text: 'Plan Estimate', dataIndex: 'PlanEstimate'},
-                {text: 'Conversation Post Text', dataIndex: 'Text',flex: 1},
-                {text: 'Conversation Post Creation Date', dataIndex: 'CreationDate', flex: 1}
+                {
+                    text: 'Plan Estimate', 
+                    dataIndex: 'PlanEstimate',
+                    style: 'textAlign:center'
+                },
+                {text: 'Last Post', flex: 2, dataIndex: 'lastPost', renderer: function(cellvalue, cell, record, idx, count, store, grid) {
+                    if ((record.posts === undefined) && (record.get('Discussion').Count > 0)) {
+                        record.getCollection('Discussion').load({
+                            fetch: ['Text', 'CreationDate'],
+                            sorters: [
+                                {
+                                    property: 'CreationDate',
+                                    direction: 'DESC'
+                                }
+                            ],
+                            callback: function (records, operation, success) {
+                                if (success) {
+                                    record.posts = records;
+                                    record.set('lastPost', records[0].get('Text'));   //If we have ordered the right way, we will have the latest first.
+                                }else {
+                                    record.set('lastPost','');
+                                }
+                            }
+                        });
+                        return 'Fetching..';
+                    } else {
+                        return record.get('lastPost');
+                    }
+                }},
+                {text: 'Last Discussion Date', dataIndex: 'LatestDiscussionAgeInMinutes', renderer: function(age) {
+                        if (age) {
+                            return Ext.Date.format(Ext.Date.subtract(new Date(), Ext.Date.MINUTE, age), 'F j, Y, g:i a').toString();
+                        }
+                        else {
+                            return null;
+                        }
+                    }
+                }
             ]
         });
          
-         }else{
-            this.grid.reconfigure(_store);
          }
+        //  else{
+        //     this.grid.reconfigure(_store);
+        //  }
     }
 
 });
